0e677680f9ceadec4881d60dceb888e07d391273
Reece <lrv378@gmail.com>
2015-01-29 16:06:30 -0700

    User sees quotes on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    @quotesArray = [
+        {author: "-Cayla Hayes", quote: "gCamp has changed my life! It's the best tool I've ever used."},
+        {author: "-Leta Jaskolski", quote: "Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been" },
+        {author: "-Laverne Upton", quote: "Don't hesitate - Sign up right now!"}
+      ]
   end
 end
+
+
+@exampleArrray = [{key: "pianos"}]
