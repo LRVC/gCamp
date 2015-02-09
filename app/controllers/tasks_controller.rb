ce4823560e75b1b5bba8b9c388259ed0eb916b4f
Reece <lrv378@gmail.com>
2015-02-09 10:57:02 -0700

    Fix error with delete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,14 +39,16 @@ class TasksController<ApplicationController
 
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def tasks_params
     params.require(:task).permit(:description)
   end
-
-
-
-
 
 end
