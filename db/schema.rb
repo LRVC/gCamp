b4784fea377dcecbfd5738499f3d92aec1cd62f8
Reece <lrv378@gmail.com>
2015-03-04 11:40:32 -0700

    Fix migration files for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224202354) do
+ActiveRecord::Schema.define(version: 20150304183814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 20150224202354) do
     t.string   "email"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password"
     t.string   "password_digest"
   end
 
