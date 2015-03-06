3163d82e6d7c48cb576df55faa54a4fa31ca704c
LRVC <lrv378@gmail.com>
2015-03-05 22:10:33 -0700

    init commit for Linux machine

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20150304183814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "add_date_to_tasks", force: :cascade do |t|
-    t.datetime "due_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
